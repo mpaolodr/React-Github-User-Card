6a10a0cc6849b6ad6d4d02fb1e740b4a20372b0e
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-14 15:14:49 -0500

    fetched data and got user info rendering

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -14,6 +14,15 @@ class App extends React.Component {
     };
   }
 
+  //fetch initial data
+  componentDidMount() {
+    fetch("https://api.github.com/users/mpaolodr")
+      //convert to json
+      .then(initialRes => initialRes.json())
+      .then(res => this.setState({ user: res }))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
@@ -21,7 +30,7 @@ class App extends React.Component {
           <h2 className="title">GitHub User Card</h2>
         </div>
         <div className="main-container">
-          <User />
+          <User user={this.state.user} />
         </div>
       </div>
     );
--- github-usercard/src/components/User.js
+++ github-usercard/src/components/User.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-const User = () => {
+const User = ({ user }) => {
   return (
     <div className="user-container">
-      <h2>user component here</h2>
+      <div className="user-card">
+        <figure className="uer-img-container">
+          <img src={user.avatar_url} alt={user.name} className="user-img" />
+        </figure>
+        <div className="user-info">
+          <h3>
+            Name: <span>{user.name}</span>
+          </h3>
+          <h3>
+            User Name: <span>{user.login}</span>
+          </h3>
+          <h3>
+            Location: <span>{user.location}</span>
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
